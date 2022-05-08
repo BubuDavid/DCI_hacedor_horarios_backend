6b8e80671b75b97437f9d64a4c04dbb0b4ca3b8c
BubuDavid <david.pedroza.segoviano@gmail.com>
2022-05-08 00:04:33 -0500

    Add update table and fix a bug

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import List
 from fastapi import FastAPI
 from decouple import config
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
 
 from tools.airtable_tools import *
 from py_scheduler import *
@@ -11,13 +12,17 @@ from py_scheduler import *
 air_api_key = config("AIRTABLE_API_KEY")
 air_base_id = config("AIRTABLE_BASE_ID")
 air_table_name = config("AIRTABLE_TABLE_NAME")
+# ====== OTHER ENV VARIABLES ====== #
+try:
+	update_password = config("PASS THE PASSWORD")
+except:
+	update_password = None
 
 app = FastAPI()
 
 # CORS POLICY CORRECTION
 origins = [
-    "http://localhost:3000",
-	"https://bubudavid.github.io/*"
+    "*"
 ]
 
 app.add_middleware(
@@ -50,3 +55,34 @@ async def get_schedule_endpoint(body: SubjectList):
 	my_subjects_json = from_subjects_to_json(validated_permutations, False)
 
 	return my_subjects_json
+
+@app.get('/update-table', response_class=HTMLResponse)
+async def update_table_endpoint():
+	if not update_password:
+		return "You need the password"
+
+	column_names = [
+		"_id",
+		"name",
+		"group",
+		"day/hour/room1",
+		"day/hour/room2",
+		"day/hour/room3",
+		"professor1",
+		"professor_email1",
+		"professor2",
+		"professor_email2",
+	]
+	url = "http://www.dci.ugto.mx/estudiantes/index.php/mcursos/horarios-licenciatura"
+	new_subjects = scrap(url, column_names)
+
+	update_airtable_table(
+		air_api_key,
+		air_base_id,
+		air_table_name,
+		new_subjects
+	)
+
+	return """ <h1>Loading...</h1>
+	<p>When this stop loading, you can get out of here</p>
+	"""
--- tools/airtable_tools.py
+++ tools/airtable_tools.py
@@ -1,6 +1,22 @@
-from pyairtable import Table
+import json
+import pyairtable as pa
+from py_scheduler import *
 
 def get_airtable_table(api_key, base_id, table_name):
-	table = Table(api_key, base_id, table_name)
+	table = pa.Table(api_key, base_id, table_name)
 	
 	return table
+
+def update_airtable_table(api_key, base_id, table_name, new_subjects):
+	table = get_airtable_table(api_key, base_id, table_name)
+	json_all_subjects = table.all()
+	for subject in json_all_subjects:
+		table.delete(subject["id"])
+
+	print("Deleted them!")
+
+	table.batch_create(new_subjects)
+
+	print("Created!")
+
+	return True
